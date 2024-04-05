e044750b5828d2d0f87b9cab52ed283bd90d6e8c
Alisson Ninomiya <53145093+alisnino@users.noreply.github.com>
2024-04-05 14:44:58 +0900

    Added EC2 and SG with EIP

--- bin/palworld-server-infra.ts
+++ bin/palworld-server-infra.ts
@@ -1,21 +1,12 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { PalworldServerInfraStack } from '../lib/palworld-server-infra-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { PalworldServerInfraStack } from "../lib/palworld-server-infra-stack";
+
+// your AWS account number and region go here.
+const env = { account: "", region: "" };
 
 const app = new cdk.App();
-new PalworldServerInfraStack(app, 'PalworldServerInfraStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new PalworldServerInfraStack(app, "PalworldServerInfraStack", {
+  env,
 });
--- lib/palworld-server-infra-stack.ts
+++ lib/palworld-server-infra-stack.ts
@@ -1,16 +1,63 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { aws_ec2, Tags } from "aws-cdk-lib";
 
 export class PalworldServerInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // tags are useful for cost allocation and resource management but are optional.
+    // Tags.of(this).add("ApplicationName", "Palworld");
+    // Tags.of(this).add("Stack", "");
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'PalworldServerInfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const vpc = new aws_ec2.Vpc(this, "PalworldVPC", {
+      maxAzs: 1,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "Public",
+          subnetType: aws_ec2.SubnetType.PUBLIC,
+        },
+      ],
+    });
+
+    // create a securityGroup with no inbound rules; your friends' IP addresses should be added in the console.
+    // you can edit this code to redeploy the stack with the correct inbound rules too, but don't forget not to upload the changes to your repository.
+    const securityGroup = new aws_ec2.SecurityGroup(
+      this,
+      "PalworldSecurityGroup",
+      {
+        vpc,
+      }
+    );
+    securityGroup.addIngressRule(
+      // Allow SSH access from the AWS EC2 Instance Connect service
+      // Remember to check the correct range for your region!
+      // https://docs.aws.amazon.com/vpc/latest/userguide/aws-ip-ranges.html
+      aws_ec2.Peer.ipv4("3.112.23.0/29"),
+      aws_ec2.Port.tcp(22),
+      "Allow SSH access from the AWS EC2 Instance Connect service"
+    );
+
+    //If you want to add your friends' IP addresses to the security group, the setting will look like this. Copy and paste this for as many friends as will play with you.
+    // securityGroup.addIngressRule(
+    //   aws_ec2.Peer.ipv4("YOUR_FRIENDS_IP")
+    //   aws_ec2.Port.tcp(8211), //Palworld required UDP
+    //   "Allow UDP access from specific IP address"
+    // );
+
+    // create an EC2 instance
+    const ec2 = new aws_ec2.Instance(this, "PalworldServer", {
+      vpc,
+      instanceType: new aws_ec2.InstanceType("t2.2xlarge"),
+      machineImage: aws_ec2.MachineImage.latestAmazonLinux2({}),
+      securityGroup,
+    });
+
+    // create ElasticIP for the server
+    const eip = new aws_ec2.CfnEIP(this, "PalworldEIP", {
+      domain: "vpc",
+      instanceId: ec2.instanceId,
+    });
   }
 }
