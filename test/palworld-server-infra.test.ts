7abf53079fbd7bbb3ff92771631ac5cfb804877e
Alisson Ninomiya <53145093+alisnino@users.noreply.github.com>
2024-04-05 12:56:16 +0900

    Initial commit with cdk init app

---
+++ test/palworld-server-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PalworldServerInfra from '../lib/palworld-server-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/palworld-server-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PalworldServerInfra.PalworldServerInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
